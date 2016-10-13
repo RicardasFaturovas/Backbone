fe2459ebf3e8306d2b30914e51bf6ed26a27a56a
RicardasFaturovas <ricardas.faturovas@gmail.com>
2016-10-13 11:24:48 +0300

    Added functionality

--- VIEWS/app.CompletedView.js
+++ VIEWS/app.CompletedView.js
@@ -3,7 +3,7 @@ app.CompletedView =  Backbone.View.extend({
     el:'#completed-todo',
     initialize: function(attrs){
           this.completed = this.$('.toggleAll');
-          this.input = this.$('.new-todo1');
+          this.input = this.$('#new-todo1');
           this.collection.on('add', this.onAdd, this);
           this.collection.on('remove', this.onRemove, this);    
           this.collection.on('reset', this.onReset,this);
@@ -12,24 +12,24 @@ app.CompletedView =  Backbone.View.extend({
           console.log(this.collection);
       },
       render: function(){
-          var done = app.todoListCollection.done();
+          var done = this.collection.done();
           this.collection = this.collection.reset(done);
           console.log(done);
       },
       events: {
-        'keypress .new-todo1': 'createTodoOnEnter',
+        'keypress #new-todo1': 'createTodoOnEnter',
         'click .toggleAll': 'toggleAllCompleted',
       },
       createTodoOnEnter: function(e){
             if ( e.which !== 13 || !this.input.val().trim() ) { // ENTER_KEY = 13
             return;
             }
-            
             this.collection.create(this.newAttributes());
             
             this.input.val(''); 
         },
         onAdd: function(todo){
+            app.todoListCollection.add(todo);
         var view = new app.TodoView({model: todo});
         $('#todo-list1').append(view.render().el);
        
